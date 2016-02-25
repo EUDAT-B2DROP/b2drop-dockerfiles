3f6ae6e18004527f0c9c0afc04dcc883f2ea98c0
Mikael Karlsson <i8myshoes@gmail.com>
2016-02-25 14:30:19 +0200

    Follow logs

--- centos/wrapper.sh
+++ centos/wrapper.sh
@@ -3,5 +3,14 @@ set -e
 
 rm -rf /run/httpd/* /tmp/httpd*
 
+tail --follow --retry \
+  /var/log/httpd/access_log \
+  /var/log/httpd/error_log \
+  /var/log/httpd/ssl_access_log \
+  /var/log/httpd/ssl_error_log \
+  /var/log/httpd/ssl_request_log \
+  /var/www/html/owncloud/data/owncloud.log \
+  &
+
 /usr/sbin/httpd -t
 exec /usr/sbin/httpd "$@"

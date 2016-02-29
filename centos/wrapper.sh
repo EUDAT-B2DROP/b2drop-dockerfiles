d367c51854467738cf9e23c6e8f890e65d382dbd
Mikael Karlsson <i8myshoes@gmail.com>
2016-02-29 10:45:57 +0200

    Install Shibboleth

--- centos/wrapper.sh
+++ centos/wrapper.sh
@@ -10,7 +10,16 @@ tail --follow --retry \
   /var/log/httpd/ssl_error_log \
   /var/log/httpd/ssl_request_log \
   /var/www/html/owncloud/data/owncloud.log \
+  /var/log/shibboleth/shibd.log \
+  /var/log/shibboleth/shibd_warn.log \
+  /var/log/shibboleth/signature.log \
+  /var/log/shibboleth/transaction.log \
+  /var/log/shibboleth-www/native.log \
+  /var/log/shibboleth-www/native_warn.log \
   &
+
+LD_LIBRARY_PATH=/opt/shibboleth/lib64 /usr/sbin/shibd -t
+LD_LIBRARY_PATH=/opt/shibboleth/lib64 /usr/sbin/shibd -f
 
 /usr/sbin/httpd -t
 exec /usr/sbin/httpd "$@"
